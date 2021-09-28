ac259482e95c220a4e433d6c529e4994d00cd0ac
Dilraj Singh <Diluppal2399@outlook.com>
2021-09-28 01:01:22 -0400

    Sep 28 1 AM

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
-module.exports = {
-  images: {
+const webpack = require('webpack');
+require('dotenv').config();
+
+ module.exports = {
+      webpack: config => {
+        const env = Object.keys(process.env).reduce((acc, curr) => {
+          acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+          return acc;
+        }, {});
+        
+        config.plugins.push(new webpack.DefinePlugin(env));
+        
+        return config;
+      }
+
+       images: {
   },
   buildModules: ['@nuxtjs/tailwindcss']
-};
+    };
+
+// module.exports = {
+//   images: {
+//   },
+//   buildModules: ['@nuxtjs/tailwindcss']
+// };
