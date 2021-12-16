0710aa82b304880d128d20cd37d9f00b7681dac6
SyedNaufil <50566955+SyedNaufil@users.noreply.github.com>
2021-12-16 01:25:18 -0500

    Create newsfeed.js

---
+++ pages/newsfeed.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import PostFeature from '../components/PostFeature'
+
+function newsfeed(){
+    return(
+        <div>
+    
+            <Header />
+            <PostFeature />
+            <Footer />
+        </div>
+    )
+}
+
+export default newsfeed
